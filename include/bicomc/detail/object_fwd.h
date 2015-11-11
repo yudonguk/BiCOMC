3812e5cd9167c81f4f3638a201da94b0b751c252
yudonguk <yudonguk@gmail.com>
2015-11-11 21:48:36 +0900

    인터페이스 해쉬 함수 변경
    * 인터페이스 해쉬 계산 함수를 SHA1에서 MurmurHash2 로 변경

--- include/bicomc/detail/object_fwd.h
+++ include/bicomc/detail/object_fwd.h
@@ -562,11 +562,7 @@ namespace detail
 			std::string data;
 			HashHelper<>::list(data);
 
-			bcc::detail::Sha1 sha1;
-			sha1.calculate(data.c_str(), data.c_str() + data.size());
-
-			bcc::array<bcc::uint32_t, 5> hash = sha1.get();
-			return *reinterpret_cast<bcc::uint64_t*>(hash.data());
+			return murmurHashNeutral64v2(data.c_str(), data.size(), 0);
 		}
 
 		static bcc::uint64_t subhash()
@@ -574,11 +570,174 @@ namespace detail
 			std::string data;
 			SubhashHelper<>::list(data);
 
-			bcc::detail::Sha1 sha1;
-			sha1.calculate(data.c_str(), data.c_str() + data.size());
+			return murmurHashNeutral64v2(data.c_str(), data.size(), 0);
+		}
 
-			bcc::array<bcc::uint32_t, 5> hash = sha1.get();
-			return *reinterpret_cast<bcc::uint64_t*>(hash.data());
+		static bcc::uint64_t getBlock(bcc::uint8_t const* data)
+		{
+			return bcc::uint64_t(data[0])
+				| bcc::uint64_t(data[1]) << 8
+				| bcc::uint64_t(data[2]) << 16
+				| bcc::uint64_t(data[3]) << 24
+				| bcc::uint64_t(data[4]) << 32
+				| bcc::uint64_t(data[5]) << 40
+				| bcc::uint64_t(data[6]) << 48
+				| bcc::uint64_t(data[7]) << 56;
+		}
+
+		static bcc::uint64_t rotl64(bcc::uint64_t x, int r)
+		{
+			return (x << r) | (x >> (64 - r));
+		}
+		
+		static bcc::uint64_t fmix(bcc::uint64_t k)
+		{
+			k ^= k >> 33;
+			k *= UINT64_C(0xFF51AFD7ED558CCD);
+			k ^= k >> 33;
+			k *= UINT64_C(0xC4CEB9FE1A85EC53);
+			k ^= k >> 33;
+			return k;
+		}
+
+		static bcc::uint64_t murmurHashNeutral64v2(void const* key, size_t len, bcc::uint64_t seed)
+		{
+			bcc::uint64_t const m = UINT64_C(0xC6A4A7935BD1E995);
+			int const r = 47;
+
+			bcc::uint64_t h = seed ^ (len * m);
+
+			bcc::uint8_t const* data = static_cast<bcc::uint8_t const*>(key);
+			bcc::uint8_t const* end = data + (len - len % sizeof(bcc::uint64_t));
+
+			for (; data != end; data += sizeof(bcc::uint64_t))
+			{
+				bcc::uint64_t k = getBlock(data);
+
+				k *= m;
+				k ^= k >> r;
+				k *= m;
+
+				h ^= k;
+				h *= m;
+			}
+
+			switch (len & 7)
+			{
+			case 7: h ^= bcc::uint64_t(data[6]) << 48;
+			case 6: h ^= bcc::uint64_t(data[5]) << 40;
+			case 5: h ^= bcc::uint64_t(data[4]) << 32;
+			case 4: h ^= bcc::uint64_t(data[3]) << 24;
+			case 3: h ^= bcc::uint64_t(data[2]) << 16;
+			case 2: h ^= bcc::uint64_t(data[1]) << 8;
+			case 1: h ^= bcc::uint64_t(data[0]);
+				h *= m;
+			};
+
+			h ^= h >> r;
+			h *= m;
+			h ^= h >> r;
+
+			return h;
+		}
+
+		static bcc::array<bcc::uint64_t, 2> murmurHashNeutral128v3(void const* key, size_t len, bcc::uint64_t seed)
+		{
+			bcc::uint8_t const* data = static_cast<bcc::uint8_t const*>(key);
+			bcc::uint8_t const* end = data + (len - len % 16);
+
+			bcc::uint64_t h1 = seed;
+			bcc::uint64_t h2 = seed;
+
+			bcc::uint64_t const c1 = UINT64_C(0x87C37B91114253D5);
+			bcc::uint64_t const c2 = UINT64_C(0x4CF5AD432745937F);
+			
+			for (; data != end; data += 16)
+			{
+				bcc::uint64_t k1 = getBlock(data);
+				bcc::uint64_t k2 = getBlock(data + sizeof(bcc::uint64_t));
+
+				k1 *= c1;
+				k1 = rotl64(k1, 31);
+				k1 *= c2;
+
+				h1 ^= k1;
+				h1 = rotl64(h1, 27);
+				h1 += h2;
+				h1 = h1 * 5 + UINT32_C(0x52DCE729);
+
+				k2 *= c2;
+				k2 = rotl64(k2, 33);
+				k2 *= c1;
+
+				h2 ^= k2;
+				h2 = rotl64(h2, 31);
+				h2 += h1;
+				h2 = h2 * 5 + UINT32_C(0x38495AB5);
+			}
+
+			bcc::uint64_t k1 = 0, k2 = 0;
+
+			switch (len & 15)
+			{
+			case 15: k2 ^= bcc::uint64_t(data[14]) << 48;
+			case 14: k2 ^= bcc::uint64_t(data[13]) << 40;
+			case 13: k2 ^= bcc::uint64_t(data[12]) << 32;
+			case 12: k2 ^= bcc::uint64_t(data[11]) << 24;
+			case 11: k2 ^= bcc::uint64_t(data[10]) << 16;
+			case 10: k2 ^= bcc::uint64_t(data[9]) << 8;
+			case 9: k2 ^= bcc::uint64_t(data[8]);
+				k2 *= c2;
+				k2 = rotl64(k2, 33);
+				k2 *= c1;
+				h2 ^= k2;
+					
+			case 8: k1 ^= bcc::uint64_t(data[7]) << 56;
+			case 7: k1 ^= bcc::uint64_t(data[6]) << 48;
+			case 6: k1 ^= bcc::uint64_t(data[5]) << 40;
+			case 5: k1 ^= bcc::uint64_t(data[4]) << 32;
+			case 4: k1 ^= bcc::uint64_t(data[3]) << 24;
+			case 3: k1 ^= bcc::uint64_t(data[2]) << 16;
+			case 2: k1 ^= bcc::uint64_t(data[1]) << 8;
+			case 1: k1 ^= bcc::uint64_t(data[0]);
+				k1 *= c1;
+				k1 = rotl64(k1, 31);
+				k1 *= c2;
+				h1 ^= k1;
+			};
+
+			h1 ^= len;
+			h2 ^= len;
+
+			h1 += h2;
+			h2 += h1;
+
+			h1 = fmix(h1);
+			h2 = fmix(h2);
+
+			h1 += h2;
+			h2 += h1;
+	
+			bcc::array<bcc::uint64_t, 2> result;
+			result[0] = h1;
+			result[1] = h2;
+			return result;
+		}
+
+		static bcc::uint64_t fnv1a64(void const* key, size_t len, bcc::uint64_t seed)
+		{
+			bcc::uint8_t const* data = static_cast<bcc::uint8_t const*>(key);
+			bcc::uint8_t const* end = data + len;
+
+			bcc::uint64_t h = seed;
+			
+			for (; data != end; ++data)
+			{
+				h ^= bcc::uint64_t(*data);
+				h *= UINT64_C(0x100000001B3);
+			}
+
+			return h;
 		}
 	};
 } // namespace detail

d0f269fb02ab92787f46a78057bc369f8e59c95e
yudonguk <yudonguk@gmail.com>
2016-05-30 02:20:38 +0900

    설정 관련 매크로를 재정의 하지 않도록 수정
    * BICOMC_IS_* 매크로를 compiler.h 로 이동
    * BICOMC_CALL, BICOMC_EXPORT, BICOMC_IS_* 매크로가 이미 정의되어 있으면, 재정의 하지 않도록 수정

--- include/bicomc/detail/compiler/clang.h
+++ include/bicomc/detail/compiler/clang.h
@@ -5,58 +5,86 @@
 #	error "compiler is not clang"
 #endif // !def __clang__
 
-#if defined(_MSC_VER) && defined(_WIN32)
-#	define BICOMC_EXPORT __declspec(dllexport)
-#else
-#	define BICOMC_EXPORT __attribute__((visibility("default")))
-#endif
+#if !defined(BICOMC_EXPORT)
+#	if defined(_MSC_VER) && defined(_WIN32)
+#		define BICOMC_EXPORT __declspec(dllexport)
+#	else
+#		define BICOMC_EXPORT __attribute__((visibility("default")))
+#	endif
+#endif // !def BICOMC_EXPORT
 
-#define BICOMC_IS_NULLPTR_SUPPORT_COMPILER \
-	(__has_feature(cxx_nullptr))
-#define BICOMC_IS_STATIC_ASSERT_SUPPORT_COMPILER \
-	(__has_feature(cxx_static_assert))
-
-#if defined(_MSC_VER) && _MSC_VER >= 1900
-#	define BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER \
-		(1)
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#	define BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER \
-		(1)
-#else
-#	define BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER \
-		(__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
-#endif
-
-#define BICOMC_IS_CONSTEXPR_SUPPORT_COMPILER \
-	(__has_feature(cxx_constexpr))
-#define BICOMC_IS_NOEXCEPT_SUPPORT_COMPILER \
-	(__has_feature(cxx_noexcept))
-#define BICOMC_IS_EXPLICITY_DEFAULT_DELETE_SUPPORT_COMPILER \
-	(__has_feature(cxx_deleted_functions))
-#define BICOMC_IS_MOVE_SEMANTIC_SUPPORT_COMPILER \
-	(__has_feature(cxx_rvalue_references))
-#define BICOMC_IS_VARIADIC_TEMPLATE_SUPPORT_COMPILER \
-	(__has_feature(cxx_variadic_templates))
-
-#if defined(_MSC_VER) && _MSC_VER >= 1900 
-#	define BICOMC_IS_CHAR_16_32_SUPPORT_COMPILER \
-		(1)
-#elif
-#	define BICOMC_IS_CHAR_16_32_SUPPORT_COMPILER \
-		(__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
-#endif
-
-#define BICOMC_IS_STD_INT_SUPPORT_COMPILER \
-	(__has_include(<cstdint>))
-#define BICOMC_IS_TYPE_TRAITS_SUPPORT_COMPILER	\
-	(__has_include(<type_traits>))
-#define BICOMC_IS_MUTEX_SUPPORT_COMPILER \
-	(__has_include(<mutex>))
-#define BICOMC_IS_CODE_CVT_UTF8_SUPPORT_COMPILER \
-	(__has_include(<codecvt>))
-#define BICOMC_IS_EXTENED_EQUAL_SUPPORT_COMPILER \
-	(__cplusplus >= 201103L)
-#define BICOMC_IS_ARRAY_SUPPORT_COMPILER \
-	(__has_include(<array>))
+#if !defined(BICOMC_IS_NULLPTR_SUPPORT_COMPILER)
+#	define BICOMC_IS_NULLPTR_SUPPORT_COMPILER \
+		(__has_feature(cxx_nullptr))
+#endif // !def BICOMC_IS_NULLPTR_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_STATIC_ASSERT_SUPPORT_COMPILER)
+#	define BICOMC_IS_STATIC_ASSERT_SUPPORT_COMPILER \
+		(__has_feature(cxx_static_assert))
+#endif // !def BICOMC_IS_STATIC_ASSERT_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER)
+#	if defined(_MSC_VER) && _MSC_VER >= 1900
+#		define BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER \
+			(1)
+#	elif defined(__GNUC__) && __GNUC__ >= 4
+#		define BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER \
+			(1)
+#	else
+#		define BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER \
+			(__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
+#	endif
+#endif // !def BICOMC_IS_THREAD_SAFE_STATIC_INIT_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_CONSTEXPR_SUPPORT_COMPILER)
+#	define BICOMC_IS_CONSTEXPR_SUPPORT_COMPILER \
+		(__has_feature(cxx_constexpr))
+#endif // !def BICOMC_IS_CONSTEXPR_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_NOEXCEPT_SUPPORT_COMPILER)
+#	define BICOMC_IS_NOEXCEPT_SUPPORT_COMPILER \
+		(__has_feature(cxx_noexcept))
+#endif // !def BICOMC_IS_NOEXCEPT_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_EXPLICITY_DEFAULT_DELETE_SUPPORT_COMPILER)
+#	define BICOMC_IS_EXPLICITY_DEFAULT_DELETE_SUPPORT_COMPILER \
+		(__has_feature(cxx_deleted_functions))
+#endif // !def BICOMC_IS_EXPLICITY_DEFAULT_DELETE_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_MOVE_SEMANTIC_SUPPORT_COMPILER)
+#	define BICOMC_IS_MOVE_SEMANTIC_SUPPORT_COMPILER \
+		(__has_feature(cxx_rvalue_references))
+#endif // !def BICOMC_IS_MOVE_SEMANTIC_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_VARIADIC_TEMPLATE_SUPPORT_COMPILER)
+#	define BICOMC_IS_VARIADIC_TEMPLATE_SUPPORT_COMPILER \
+		(__has_feature(cxx_variadic_templates))
+#endif // !def BICOMC_IS_VARIADIC_TEMPLATE_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_CHAR_16_32_SUPPORT_COMPILER)
+#	if defined(_MSC_VER) && _MSC_VER >= 1900 
+#		define BICOMC_IS_CHAR_16_32_SUPPORT_COMPILER \
+			(1)
+#	elif
+#		define BICOMC_IS_CHAR_16_32_SUPPORT_COMPILER \
+			(__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
+#	endif
+#endif // !def BICOMC_IS_CHAR_16_32_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_STD_INT_SUPPORT_COMPILER)
+#	define BICOMC_IS_STD_INT_SUPPORT_COMPILER \
+		(__has_include(<cstdint>))
+#endif // !def BICOMC_IS_STD_INT_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_TYPE_TRAITS_SUPPORT_COMPILER)
+#	define BICOMC_IS_TYPE_TRAITS_SUPPORT_COMPILER	\
+		(__has_include(<type_traits>))
+#endif // !def BICOMC_IS_TYPE_TRAITS_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_MUTEX_SUPPORT_COMPILER)
+#	define BICOMC_IS_MUTEX_SUPPORT_COMPILER \
+		(__has_include(<mutex>))
+#endif // !def BICOMC_IS_MUTEX_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_CODE_CVT_UTF8_SUPPORT_COMPILER)
+#	define BICOMC_IS_CODE_CVT_UTF8_SUPPORT_COMPILER \
+		(__has_include(<codecvt>))
+#endif // !def BICOMC_IS_CODE_CVT_UTF8_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_EXTENED_EQUAL_SUPPORT_COMPILER)
+#	define BICOMC_IS_EXTENED_EQUAL_SUPPORT_COMPILER \
+		(__cplusplus >= 201103L)
+#endif // !def BICOMC_IS_EXTENED_EQUAL_SUPPORT_COMPILER
+#if !defined(BICOMC_IS_ARRAY_SUPPORT_COMPILER)
+#	define BICOMC_IS_ARRAY_SUPPORT_COMPILER \
+		(__has_include(<array>))
+#endif // !def BICOMC_IS_ARRAY_SUPPORT_COMPILER
 
 #endif // !def BICOMC_DETAIL_COMPILER_CLANG_H__
